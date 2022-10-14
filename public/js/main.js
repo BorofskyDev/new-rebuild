717a8c7e962a279aac42ff6b75b928556676ee3e
solovyovsdom <joelborofsky@mac.com>
2022-10-13 22:42:48 -0500

    added contact page

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,9 @@ import '../styles/hero.css'
 import '../styles/about.css'
 import '../styles/features.css'
 import '../styles/floorplans.css'
+import '../styles/fees.css'
+import '../styles/contact.css'
 
 import './navbar.js'
+import './contact.js'
 
